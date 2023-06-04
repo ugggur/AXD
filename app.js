d08085459c67195909ffb8a39b5dfc24b4b2c92c
ugggur <ugurcaglar@gmail.com>
2023-06-04 14:03:38 +0200

    many changes

--- app.js
+++ app.js
@@ -4,3 +4,17 @@ btn.addEventListener("click", function (e) {
   console.log(e);
   alert("welcome");
 });
+
+const form= document.getElementById("email-form");
+form.addEventListener("submit", function(e){
+  
+  const formData = new FormData(form);
+  const email = formData.get("email");
+  console.log("email", email);
+
+})
+
+function changeColor(){
+  var divElement = document.getElementById('myBody');
+  divElement.style.backgroundColor = 'red';
+}
